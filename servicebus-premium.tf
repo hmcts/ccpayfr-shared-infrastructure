d61ae7e80a9116ecde29fe30944e071a4ad363bb
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-11-24 08:16:29 +0000

    Create servicebus-premium.tf

---
+++ servicebus-premium.tf
@@ -0,0 +1,69 @@
+locals {
+  subscription_name = "defaultServiceCallbackSubscription"
+  retry_queue       = "serviceCallbackRetryQueue"
+}
+
+module "servicebus-namespace-premium" {
+  providers = {
+    azurerm.private_endpoint = azurerm.private_endpoint
+  }
+  
+  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
+  name                = "${var.product}-servicebus-${var.env}"
+  location            = var.location
+  env                 = var.env
+  common_tags         = local.tags
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Premium"
+  zone_redundant      = true
+  capacity            = 1
+
+}
+
+module "topic-premium" {
+  source                = "git@github.com:hmcts/terraform-module-servicebus-topic?ref=master"
+  name                  = "serviceCallbackTopic"
+  namespace_name        = module.servicebus-namespace-premium.name
+  resource_group_name   = azurerm_resource_group.rg.name
+}
+
+module "queue-premium" {
+  source                = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
+  name                  = local.retry_queue
+  namespace_name        = module.servicebus-namespace-premium.name
+  resource_group_name   = azurerm_resource_group.rg.name
+}
+
+module "subscription-premium" {
+  source                = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
+  name                  = local.subscription_name
+  namespace_name        = module.servicebus-namespace-premium.name
+  topic_name            = module.topic-premium.name
+  resource_group_name   = azurerm_resource_group.rg.name
+  max_delivery_count    = "1"
+  forward_dead_lettered_messages_to = module.queue-premium.name
+}
+
+
+
+resource "azurerm_key_vault_secret" "servicebus_primary_connection_string-premium" {
+  name         = "sb-primary-connection-string"
+  value        = module.servicebus-namespace-premium.primary_send_and_listen_connection_string
+  key_vault_id = data.azurerm_key_vault.ccpay_key_vault.id
+}
+
+# primary connection string for send and listen operations
+output "sb_primary_send_and_listen_connection_string-premium" {
+  value     = module.servicebus-namespace-premium.primary_send_and_listen_connection_string
+  sensitive = true
+}
+
+output "topic_primary_send_and_listen_connection_string-premium" {
+  value     = module.topic-premium.primary_send_and_listen_connection_string
+  sensitive = true
+}
+
+output "psc_subscription_connection_string" {
+  value     = "${module.topic.primary_send_and_listen_connection_string}/subscriptions/${local.subscription_name}"
+  sensitive = true
+}
