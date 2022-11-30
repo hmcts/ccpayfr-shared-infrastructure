2d3ddc9c8598e574b97d19b89de8a9d18c824a19
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-11-30 14:12:11 +0000

    Update emailAlert.tf

--- emailAlert.tf
+++ emailAlert.tf
@@ -11,7 +11,7 @@ module "feepay-fail-alert" {
   frequency_in_minutes       = 15
   time_window_in_minutes     = 15
   severity_level             = "3"
-  action_group_name          = module.feepay-fail-action-group-slack.action_group_name
+  action_group_name          = module.feepay-fail-action-group.action_group_name
   custom_email_subject       = "feepay Service Exception"
   trigger_threshold_operator = "GreaterThan"
   trigger_threshold          = 0
@@ -20,14 +20,14 @@ module "feepay-fail-alert" {
 }
 
 
-module "feepay-fail-action-group-slack" {
+module "feepay-fail-action-group" {
   source   = "git@github.com:hmcts/cnp-module-action-group"
   location = "global"
   env      = var.env
 
   resourcegroup_name     = azurerm_resource_group.rg.name
-  action_group_name      = "feepay Fail Slack Alert - ${var.env}"
-  short_name             = "feepay_slack"
+  action_group_name      = "feepay Fail  Alert - ${var.env}"
+  short_name             = "feepay_alert"
   email_receiver_name    = "feepay Alerts"
   email_receiver_address = "anshika.nigam@hmcts.net"
 }
